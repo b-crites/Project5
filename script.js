f0908107517af39cf98e0fe3a434535cde56d99c
Brandon Crites <brandoncrites76@gmail.com>
2023-02-26 20:08:56 -0800

    Time

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-const events = document.querySelectorAll('.time-block')
+// const events = document.querySelectorAll('.time-block')
+const currentDayEl = document.getElementById('currentDay')
   // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
   // the code isn't run until the browser has finished rendering all the elements
   // in the html.
   
-$('button').click(function () {
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
@@ -22,5 +24,11 @@ $('button').click(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+ 
 
-});
+  
+  var dayVar = dayjs();
+  var dayVarSubtracted = dayVar.subtract(8, 'h')
+  currentDayEl.innerHTML = dayjs();
+  console.log(dayjs());
+// });
