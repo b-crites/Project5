62d120059583cdc9d2966c9d9454a54f6a68e427
Brandon Crites <brandoncrites76@gmail.com>
2023-03-13 15:55:26 -0700

    Local Storage

--- script.js
+++ script.js
@@ -2,18 +2,16 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 
+// TODO: Add a listener for click events on the save button. This code should
+// use the id in the containing time-block as a key to save the user input in
+// local storage. HINT: What does `this` reference in the click listener
+// function? How can DOM traversal be used to get the "hour-x" id of the
+// time-block containing the button that was clicked? How might the id be
+// useful when saving the description in local storage?
+var currentDayEl = document.getElementById('currentDay')
 $(document).ready(function(){
-const currentDayEl = document.getElementById('currentDay')
-var dayHours = ['9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM']
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  $('.saveBtn').each(function () {
-
-    $(this).on('click', function () {
+    
+    $(".saveBtn").on('click', function () {
       // Get input text and hour id
       var inputText = $(this).siblings('.description').val();
       var hour = $(this).parent().attr('id');
@@ -21,30 +19,44 @@ var dayHours = ['9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM'
       // store input text in local storage as long as its not empty
       if (inputText.trim() !== '') {
         localStorage.setItem(hour, inputText);
-        //notifies user that the save was successful
-        alert('Event saved.');
       }
+      
     });
-  });
+  
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-    if(dayHours < dayVar){
-      $("div").addClass('past');
-      $("div").removeClass('present')
-      $("div").removeClass('future')
-    }else if(dayHours === dayVar){
-      $("div").addClass('present');
-      $("div").removeClass('past')
-      $("div").removeClass('future')
-    }else{
-      $("div").addClass('future');
-      $("div").removeClass('past')
-      $("div").removeClass('present')
-    }
+  function timeTracker() {
+    //get current number of hours.
+    
+
+    // loop over time blocks
+    $(".time-block").each(function () {
+        var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+        // To check the time and add the classes for background indicators
+        if (blockTime < dayVar) {
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+            $(this).addClass("past");
+        }
+        else if (blockTime === dayVar) {
+            $(this).removeClass("past");
+            $(this).removeClass("future");
+            $(this).addClass("present");
+        }
+        else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+
+        }
+    })
+}
+
 
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
@@ -52,9 +64,22 @@ var dayHours = ['9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM'
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
- 
+  
   var dayVar = dayjs();
-  var dayVarSubtracted = dayVar.subtract(8, 'h')
+  var dayVarSubtracted = dayVar.subtract(7, 'h')
   currentDayEl.innerHTML = dayVarSubtracted;
   console.log(dayjs());
+  // Local Storage
+  $("#hour9 .description").val(localStorage.getItem("hour9"));
+  $("#hour10 .description").val(localStorage.getItem("hour10"));
+  $("#hour11 .description").val(localStorage.getItem("hour11"));
+  $("#hour12 .description").val(localStorage.getItem("hour12"));
+  $("#hour1 .description").val(localStorage.getItem("hour1"));
+  $("#hour2 .description").val(localStorage.getItem("hour2"));
+  $("#hour3 .description").val(localStorage.getItem("hour3"));
+  $("#hour4 .description").val(localStorage.getItem("hour4"));
+  $("#hour5 .description").val(localStorage.getItem("hour5"));
+  
+  timeTracker();
 });
+
